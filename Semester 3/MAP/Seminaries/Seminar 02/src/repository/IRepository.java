380fb6b9f5e41d7360f1a3e8773eefa44070ed4c
Grecu Narcis <narcisgrecu07@gmail.com>
2023-12-17 17:20:18 +0200

    update

--- Semester 3/MAP/Seminaries/Seminar 02/src/repository/IRepository.java
+++
@@ -1,9 +0,0 @@
-package repository;
-
-import models.Item;
-public interface IRepository {
-    void addItem(Item itemToAdd);
-    // void removeItem(Item itemToRemove);
-    Item[] getAll();
-    int getSize();
-}
